08431ba61ddfcb4918f052754a5171a96edb1d2f
johan <t511069160@qq.com>
2023-07-08 19:32:16 +0800

    vue格式化

--- docs/.vitepress/theme/components/RoadMap.vue
+++ docs/.vitepress/theme/components/RoadMap.vue
@@ -1,58 +1,57 @@
 <template>
-
     <div class="map-container">
-      <div v-if="showImg">
-        <p>
-          移动端长图绘制较慢，显示静态图片，大家请在PC端打开
-        </p>
-      </div>
-      <div v-else>
-        <p v-if="isIndex">
-          绿色的方块都可以点击进入详情
-        </p>
-        <button class="download-btn" @click="downloadCanvas">导出图片</button>
-        <canvas ref="canvasRef" :height="height" width="780" />
-      </div>
+        <div v-if="showImg">
+            <p>
+                移动端长图绘制较慢，显示静态图片，大家请在PC端打开
+            </p>
+        </div>
+        <div v-else>
+            <p v-if="isIndex">
+                绿色的方块都可以点击进入详情
+            </p>
+            <button class="download-btn" @click="downloadCanvas">导出图片</button>
+            <canvas ref="canvasRef" :height="height" width="780" />
+        </div>
     </div>
-  </template>
+</template>
 
-  <script setup>
-  import UAParser from 'ua-parser-js'
-  import { ref, onMounted, onUnmounted,computed } from 'vue'
-  import {useRoute} from 'vue-router'
-  import { drawMap } from './utils'
-  const props = defineProps({
+<script setup>
+import UAParser from 'ua-parser-js'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { drawMap } from './utils'
+const props = defineProps({
     height: Number,
     data: Object
-  })
-  let canvasRef = ref()
-  const route = useRoute()
-  let parser = new UAParser()
-  let result = parser.getResult()
+})
+let canvasRef = ref()
+const route = useRoute()
+let parser = new UAParser()
+let result = parser.getResult()
 //   console.log('route', route)
-  const isIndex = ref(route?.path==='/')
-  const showImg=ref(result.device.type&&isIndex.value)
+const isIndex = ref(route?.path === '/')
+const showImg = ref(result.device.type && isIndex.value)
 
-  function canvasClick(e) {
-    if(e.target){
-      if(e.target.download){
-        downloadCanvas()
-      }
-      if(e.target.link){
-        location.href = e.target.link
-      }
+function canvasClick(e) {
+    if (e.target) {
+        if (e.target.download) {
+            downloadCanvas()
+        }
+        if (e.target.link) {
+            location.href = e.target.link
+        }
     }
-  }
+}
 
-  function downloadCanvas() {
+function downloadCanvas() {
     const canvas = canvasRef.value
     // console.log('canvas', canvas)
     const dataURL = canvas.toDataURL({
-      width: canvas.width,
-      height: canvas.height,
-      left: 0,
-      top: 0,
-      format: 'png',
+        width: canvas.width,
+        height: canvas.height,
+        left: 0,
+        top: 0,
+        format: 'png',
     })
     const link = document.createElement('a');
     link.download = `${canvas.title}-${Date.now()}.png`;
@@ -60,39 +59,42 @@
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-  }
-  onMounted(() => {
-    if(!showImg.value){
-      const canvas = drawMap(canvasRef.value, props.data)
-      canvas.on("mouse:down", canvasClick)
+}
+onMounted(() => {
+    if (!showImg.value) {
+        const canvas = drawMap(canvasRef.value, props.data)
+        canvas.on("mouse:down", canvasClick)
     }
     if (import.meta.hot) {
-    // HMR 代码
-      console.log('热更新？')
+        // HMR 代码
+        console.log('热更新？')
     }
-  })
+})
 
-  </script>
+</script>
 
-  <style>
-  .map-container{
+<style>
+.map-container {
     position: relative;
-  }
-  .map-container p{
-    font-size:12px;
+}
+
+.map-container p {
+    font-size: 12px;
     text-align: center;
 
-    margin:0;
-  }
-  h1{
+    margin: 0;
+}
+
+h1 {
     text-align: center;
-  }
-  .download-btn{
+}
+
+.download-btn {
     position: absolute;
     right: -40px;
-    top:-20px;
+    top: -20px;
     display: inline-block;
-    margin-left:36px;
+    margin-left: 36px;
     line-height: 1;
     white-space: nowrap;
     cursor: pointer;
@@ -109,5 +111,5 @@
     background: 0 0;
     padding-left: 0;
     padding-right: 0;
-  }
-  </style>
+}
+</style>
